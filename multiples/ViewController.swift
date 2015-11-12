ffb069df727744399cc3cf93fccd33a1c463f49e
Julien Guanzon <julienguanzon@gmail.com>
2015-11-12 14:06:38 -0700

    initial commit

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentSum: Int!
+    var newSum: Int!
+    var multipleSelected: Int!
 
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var multipleTextField: UITextField!
+    @IBOutlet weak var multiplesImageView: UIImageView!
+    
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var solutionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +31,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    
+    @IBAction func playBtnTapped(sender: AnyObject) {
+        multipleSelected = Int(multipleTextField.text!)
+        if multipleSelected != nil && multipleSelected > 0 {
+            playBtn.hidden = true
+            multiplesImageView.hidden = true
+            multipleTextField.hidden = true
+            
+            instructionLabel.hidden = false
+            addBtn.hidden = false
+        } else {
+            return
+        }
+    }
+    
+    
+    @IBAction func addBtnTapped(sender: AnyObject) {
+        if !instructionLabel.hidden {
+            instructionLabel.hidden = true
+            solutionLabel.hidden = false
+            currentSum = 0
+            solutionLabel.text = updatedSolution()
+            currentSum = newSum
+        } else {
+            solutionLabel.text = updatedSolution()
+            currentSum = newSum
+        }
+    }
+    
+    func updatedSolution () ->String {
+        newSum = currentSum + multipleSelected
+        return "\(currentSum) + \(multipleSelected) = \(newSum)"
+    }
+
 
 
 }
